503d3d9fb2f242bbc9d1aa23b6a61a0125d24797
ElenaShylionak <99590421+ElenaShylionak@users.noreply.github.com>
2023-02-13 14:15:02 +0300

    Creating imaginary database to work

---
+++ src/main/java/org/itstep/mvc/model/User.java
@@ -0,0 +1,32 @@
+package org.itstep.mvc.model;
+
+
+//Java Persistence — спецификация, предоставляет возможность сохранять в удобном виде Java-объекты в базе данных.
+//Технология Jakarta EE, до 2018 года известная как Java EE, помогает соединить старые и новые технологии
+// и позволяет приложениям эффективно взаимодействовать
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table (name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (name = "user_name")
+    String userName;
+    @Column (name = "user_lastname")
+    String userLastName;
+    @Column (name = "email")
+    String email;
+
+
+}
